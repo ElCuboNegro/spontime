3e5c9f48554fdcc862de54db5b72361b17427330
Juan Jose Alban Ortiz <j@wizzysage.com>
2024-09-30 00:03:44 -0500

    Initialize release/api_working

---
+++ challenge/model.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from typing import Tuple, Union, List
+
+class DelayModel:
+
+    def __init__(self):
+        self._model = None # Model should be saved in this attribute.
+
+    def preprocess(
+        self,
+        data: pd.DataFrame,
+        target_column: str = None
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
+        """
+        Prepare raw data for training or predict.
+
+        Args:
+            data (pd.DataFrame): raw data.
+            target_column (str, optional): if set, the target is returned.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
+            or
+            pd.DataFrame: features.
+        """
+        return data
+
+    def fit(
+        self,
+        features: pd.DataFrame,
+        target: pd.DataFrame
+    ) -> None:
+        """
+        Fit model with preprocessed data.
+
+        Args:
+            features (pd.DataFrame): preprocessed data.
+            target (pd.DataFrame): target.
+        """
+        return
+
+    def predict(
+        self,
+        features: pd.DataFrame
+    ) -> List[int]:
+        """
+        Predict delays for new flights.
+
+        Args:
+            features (pd.DataFrame): preprocessed data.
+        
+        Returns:
+            (List[int]): predicted targets.
+        """
+        preprocessed_data = self.preprocess(features)
+        return [0] * preprocessed_data.shape[0]  # Dummy prediction
